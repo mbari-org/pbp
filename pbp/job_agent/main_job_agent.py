83542ab3e25bb26e7b4c82decb10c90a1f881e79
Michael.C Ryan <spacetime.engineer@gmail.com>
2024-11-20 15:29:03 -0500

    Improved argument structure
    Improved argument structure

--- pbp/job_agent/main_job_agent.py
+++ pbp/job_agent/main_job_agent.py
@@ -1,45 +1,36 @@
 from pbp.job_agent.main_job_agent_args import parse_arguments
 
-
 def main():
     
     opts = parse_arguments()
 
-    from pbp.logging_helper import create_logger
     from pbp.job_agent.job_agent import JobAgent
-    
-    log = create_logger(
-        log_filename_and_level=(
-            f"{opts.log_dir}/{opts.prefix}{opts.start}_{opts.end}.log",
-            "INFO",
-        ),
-        console_level="WARNING",
-    )
 
     job_agent = JobAgent(
-        output_prefix=opts.output_prefix,
-        recorder=opts.recorder,
-        audio_base_dir=opts.audio_base_dir,
-        json_base_dir=opts.json_base_dir,
-        start=opts.start,
-        end=opts.end,
-        prefix=opts.prefix,
-        nc_output_dir=opts.nc_output_dir,
-        global_attrs=opts.global_attrs,
-        variable_attrs=opts.variable_attrs,
-        sensitivity_flat_value=opts.sensitivity_flat_value,
-        latlon=opts.latlon,
-        title=opts.title,
-        cmlim=opts.cmlim,
-        ylim=opts.ylim,
-        log_dir = opts.log_dir,
-        meta_output_dir = opts.meta_output_dir,
-        xml_dir = opts.xml_dir)
+        output_prefix=opts["output_prefix"],
+        recorder=opts["recorder"],
+        audio_base_dir=opts["audio_base_dir"],
+        json_base_dir=opts["json_base_dir"],
+        start=opts["start"],
+        end=opts["end"],
+        prefix=opts["prefix"],
+        nc_output_dir=opts["nc_output_dir"],
+        global_attrs=opts["global_attrs"],
+        variable_attrs=opts["variable_attrs"],
+        sensitivity_flat_value=opts["sensitivity_flat_value"],
+        latlon=opts["latlon"],
+        title=opts["title"],
+        cmlim=opts["cmlim"],
+        ylim=opts["ylim"],
+        log_dir=opts["log_dir"],
+        meta_output_dir=opts["meta_output_dir"],
+        xml_dir=opts["xml_dir"]
+    )
     
     try:
         job_agent.run()
-    except KeyboardInterrupt:
-        log.info("INTERRUPTED")
+    except KeyboardInterrupt as error:
+        print(f"Operation was interrupted by the user: {error}")
 
 if __name__ == "__main__":
     main()
--- pbp/job_agent/main_job_agent_args.py
+++ pbp/job_agent/main_job_agent_args.py
@@ -3,8 +3,14 @@ from pbp.meta_gen.utils import InstrumentType
 
 from pbp import get_pbp_version
 from pbp.job_agent.utils import yaml_to_json
+from loguru import logger
+import sys
+import os
 
 def parse_arguments():
+    
+
+    
     description = "Process ocean audio data archives to daily analysis products of hybrid millidecade spectra using PyPAM."
     example = """
     Examples:
@@ -22,262 +28,104 @@ def parse_arguments():
         type=str,
         metavar="uri",
         default=None,
+        nargs='+',
+        required=True,
         help="URI of JSON file with global attributes to be added to the NetCDF file.",
     )
 
     if parser.parse_args().global_attrs is not None: # Checks if there is even a yaml file provided.
-        yaml_path =  parser.parse_args().global_attrs # The yaml_path is the path to the yaml file but it is not known yet if the value exists in the yaml.
-        if yaml_path is not None: # If there is a yaml path is provided.
-            yaml_data = yaml_to_json(yaml_path) # Convert the yaml file to a parsable object. This parsing is yaml based and distinct from the Argparse parsing.
-            if yaml_data["pbp_job_agent"]["output_prefix"] is not None: # If the yaml file has a recorder key present.
-                parser.add_argument(
-                    "--output_prefix",
-                    choices=[InstrumentType.NRS, InstrumentType.ICLISTEN, InstrumentType.SOUNDTRAP],
-                    required=False,
-                    default=yaml_data["pbp_job_agent"]["output_prefix"],
-                    help="Choose the audio instrument type",
-                )
-            else:
-                parser.add_argument(
-                    "--recorder",
-                    choices=[InstrumentType.NRS, InstrumentType.ICLISTEN, InstrumentType.SOUNDTRAP],
-                    required=True,
-                    help="Choose the audio instrument type",
-                )
-            
-            if yaml_data["pbp_job_agent"]["recorder"] is not None: # If the yaml file has a recorder key present.
-                parser.add_argument(
-                    "--recorder",
-                    choices=[InstrumentType.NRS, InstrumentType.ICLISTEN, InstrumentType.SOUNDTRAP],
-                    required=False,
-                    default=yaml_data["pbp_job_agent"]["recorder"],
-                    help="Choose the audio instrument type",
-                )
-            else:
-                parser.add_argument(
-                    "--recorder",
-                    choices=[InstrumentType.NRS, InstrumentType.ICLISTEN, InstrumentType.SOUNDTRAP],
-                    required=True,
-                    help="Choose the audio instrument type",
-                )
+        
+        for deployment_configuration in parser.parse_args().global_attrs: # Iterates through the yaml files provided.
+            if deployment_configuration is not None: # If there is a yaml path is provided.
+                yaml_data = yaml_to_json(deployment_configuration) # Convert the yaml file to a parsable object. This parsing is yaml based and distinct from the Argparse parsing.
+                if yaml_data["pbp_job_agent"]["output_prefix"] is not None: # If the yaml file has a recorder key present.
+                    pass
+                else:
+                    logger.error("The 'output_prefix' key-value pair in the --global-attrs YAML file(s) is necessary to run the job agent.")
+                    exit(1)
                 
-            if yaml_data["pbp_job_agent"]["log_dir"] is not None: # If the yaml file has a recorder key present.
-                parser.add_argument(
-                    "--log_dir",
-                    required=False,
-                    default=yaml_data["pbp_job_agent"]["log_dir"],
-                    help="The path where the job agent log file will be saved.",
-                )
-            else:
-                parser.add_argument(
-                    "--log_dir",
-                    required=True,
-                    help="The path where the job agent log file will be saved.",
-                )
-                
-            if yaml_data["pbp_job_agent"]["prefix"] is not None: # If the yaml file has a recorder key present.
-                parser.add_argument(
-                    "--prefix",
-                    required=False,
-                    metavar="prefix",
-                    default=yaml_data["pbp_job_agent"]["prefix"],
-                    help="The path where the job agent log file will be saved.",
-                )
-            else:
-                parser.add_argument(
-                    "--prefix",
-                    required=True,
-                    metavar="prefix",
-                    help="The path where the job agent log file will be saved.",
-                )
-                
-            if yaml_data["pbp_job_agent"]["start"] is not None: # If the yaml file has a recorder key present.
-                parser.add_argument(
-                    "--start",
-                    required=False,
-                    metavar="start date",
-                    default=yaml_data["pbp_job_agent"]["start"],
-                    help="The desired starting date of the deployment period for processing.",
-                )
-            else:
-                parser.add_argument(
-                    "--start",
-                    required=True,
-                    metavar="start date",
-                    help="The desired starting date of the deployment period for processing.",
-                )
-            if yaml_data["pbp_job_agent"]["end"] is not None: # If the yaml file has a recorder key present.
-                parser.add_argument(
-                    "--end",
-                    required=False,
-                    metavar="start date",
-                    default=yaml_data["pbp_job_agent"]["end"],
-                    help="The desired starting date of the deployment period for processing.",
-                )
-            else:
-                parser.add_argument(
-                    "--end",
-                    required=True,
-                    metavar="start date",
-                    help="The desired starting date of the deployment period for processing.",
-                )
-            if yaml_data["pbp_job_agent"]["audio_base_dir"] is not None: # If the yaml file has a recorder key present.
-                parser.add_argument(
-                    "--audio_base_dir",
-                    required=False,
-                    default=yaml_data["pbp_job_agent"]["audio_base_dir"],
-                    help="",
-                )
-            else:
-                parser.add_argument(
-                    "--audio_base_dir",
-                    required=True,
-                    help="",
-                )
-            if yaml_data["pbp_job_agent"]["json_base_dir"] is not None: # If the yaml file has a recorder key present.
-                parser.add_argument(
-                    "--json_base_dir",
-                    required=False,
-                    default=yaml_data["pbp_job_agent"]["json_base_dir"],
-                    help="",
-                )
-            else:
-                parser.add_argument(
-                    "--json_base_dir",
-                    required=True,
-                    help="",
-                )
-            if yaml_data["pbp_job_agent"]["xml_dir"] is not None: # If the yaml file has a recorder key present.
-                parser.add_argument(
-                    "--xml_dir",
-                    required=False,
-                    default=yaml_data["pbp_job_agent"]["xml_dir"],
-                    help="",
-                )
-            else:
-                parser.add_argument(
-                    "--xml_dir",
-                    required=True,
-                    help="",
-                )
-            if yaml_data["pbp_job_agent"]["nc_output_dir"] is not None: # If the yaml file has a recorder key present.
-                parser.add_argument(
-                    "--nc_output_dir",
-                    required=False,
-                    default=yaml_data["pbp_job_agent"]["nc_output_dir"],
-                    help="",
-                )
-            else:
-                parser.add_argument(
-                    "--nc_output_dir",
-                    required=True,
-                    help="",
-                )
-            if yaml_data["pbp_job_agent"]["variable_attrs"] is not None: # If the yaml file has a recorder key present.
-                parser.add_argument(
-                    "--variable_attrs",
-                    default=yaml_data["pbp_job_agent"]["variable_attrs"],
-                    required=False,
-                    help="",
-                )
-            else:
-                parser.add_argument(
-                    "--variable_attrs",
-                    required=True,
-                    help="",
-                )
-            if yaml_data["pbp_job_agent"]["sensitivity_flat_value"] is not None: # If the yaml file has a recorder key present.
-                parser.add_argument(
-                    "--sensitivity_flat_value",
-                    required=False,
-                    default=yaml_data["pbp_job_agent"]["sensitivity_flat_value"],
-                    help="",
-                )
-            else:
-                parser.add_argument(
-                    "--sensitivity_flat_value",
-                    required=True,
-                    help="",
-                )
-            if yaml_data["pbp_job_agent"]["latlon"] is not None: # If the yaml file has a recorder key present.
-                parser.add_argument(
-                    "--latlon",
-                    required=False,
-                    default=yaml_data["pbp_job_agent"]["latlon"],
-                    help="",
-                )
-            else:
-                parser.add_argument(
-                    "--latlon",
-                    required=True,
-                    help="",
-                )
-            if yaml_data["pbp_job_agent"]["title"] is not None: # If the yaml file has a recorder key present.
-                parser.add_argument(
-                    "--title",
-                    required=False,
-                    default=yaml_data["pbp_job_agent"]["title"],
-                    help="",
-                )
-            else:
-                parser.add_argument(
-                    "--title",
-                    required=True,
-                    help="",
-                )
-            if yaml_data["pbp_job_agent"]["cmlim"] is not None: # If the yaml file has a recorder key present.
-                parser.add_argument(
-                    "--cmlim",
-                    required=False,
-                    default=yaml_data["pbp_job_agent"]["cmlim"],
-                    help="",
-                )
-            else:
-                parser.add_argument(
-                    "--cmlim",
-                    required=True,
-                    help="",
-                )
-            if yaml_data["pbp_job_agent"]["ylim"] is not None: # If the yaml file has a recorder key present.
-                parser.add_argument(
-                    "--ylim",
-                    required=False,
-                    default=yaml_data["pbp_job_agent"]["ylim"],
-                    help="",
-                )
-            else:
-                parser.add_argument(
-                    "--ylim",
-                    required=True,
-                    help="",
-                )
-            if yaml_data["pbp_job_agent"]["meta_output_dir"] is not None: # If the yaml file has a recorder key present.
-                parser.add_argument(
-                    "--meta_output_dir",
-                    required=False,
-                    default=yaml_data["pbp_job_agent"]["meta_output_dir"],
-                    help="",
-                )
-            else:
-                parser.add_argument(
-                    "--meta_output_dir",
-                    required=True,
-                    help="",
-                )
-
-
-            
-            
-
-
-    parser.add_argument(
-        "--version",
-        action="version",
-        version=get_pbp_version(),
-    )
-    
-    
-
-
-
-    return parser.parse_args()
+                if yaml_data["pbp_job_agent"]["recorder"] is not None: # If the yaml file has a recorder key present.
+                    pass
+                else:
+                    logger.error("The 'recorder' key-value pair in the --global-attrs YAML file(s) is necessary to run the job agent.")
+                    exit(1)
+                    
+                if yaml_data["pbp_job_agent"]["log_dir"] is not None: # If the yaml file has a recorder key present.
+                    pass
+                else:
+                    logger.error("The 'log_dir' key-value pair in the --global-attrs YAML file(s) is necessary to run the job agent.")
+                    exit(1)
+                    
+                if yaml_data["pbp_job_agent"]["prefix"] is not None: # If the yaml file has a recorder key present.
+                    pass
+                else:
+                    logger.error("The 'prefix' key-value pair in the --global-attrs YAML file(s) is necessary to run the job agent.")
+                    exit(1)
+                    
+                if yaml_data["pbp_job_agent"]["start"] is not None: # If the yaml file has a recorder key present.
+                    pass
+                else:
+                    logger.error("The 'start' key-value pair in the --global-attrs YAML file(s) is necessary to run the job agent.")
+                    exit(1)
+                if yaml_data["pbp_job_agent"]["end"] is not None: # If the yaml file has a recorder key present.
+                    pass
+                else:
+                    logger.error("The 'end' key-value pair in the --global-attrs YAML file(s) is necessary to run the job agent.")
+                    exit(1)
+                if yaml_data["pbp_job_agent"]["audio_base_dir"] is not None: # If the yaml file has a recorder key present.
+                    pass
+                else:
+                    logger.error("The 'audio_base_dir' key-value pair in the --global-attrs YAML file(s) is necessary to run the job agent.")
+                    exit(1)
+                if yaml_data["pbp_job_agent"]["json_base_dir"] is not None: # If the yaml file has a recorder key present.
+                    pass
+                else:
+                    logger.error("The 'json_base_dir' key-value pair in the --global-attrs YAML file(s) is necessary to run the job agent.")
+                    exit(1)
+                if yaml_data["pbp_job_agent"]["xml_dir"] is not None: # If the yaml file has a recorder key present.
+                    pass
+                else:
+                    logger.error("The 'xml_dir' key-value pair in the --global-attrs YAML file(s) is necessary to run the job agent.")
+                    exit(1)
+                if yaml_data["pbp_job_agent"]["nc_output_dir"] is not None: # If the yaml file has a recorder key present.
+                    pass
+                else:
+                    logger.error("The 'nc_output_dir' key-value pair in the --global-attrs YAML file(s) is necessary to run the job agent.")
+                    exit(1)
+                if yaml_data["pbp_job_agent"]["variable_attrs"] is not None: # If the yaml file has a recorder key present.
+                    pass
+                else:
+                    logger.error("The 'variable_attrs' key-value pair in the --global-attrs YAML file(s) is necessary to run the job agent.")
+                    exit(1)
+                if yaml_data["pbp_job_agent"]["sensitivity_flat_value"] is not None: # If the yaml file has a recorder key present.
+                    pass
+                else:
+                    logger.error("The 'sensitivity_flat_value' key-value pair in the --global-attrs YAML file(s) is necessary to run the job agent.")
+                    exit(1)
+                if yaml_data["pbp_job_agent"]["latlon"] is not None: # If the yaml file has a recorder key present.
+                    pass
+                else:
+                    logger.error("The 'latlon' key-value pair in the --global-attrs YAML file(s) is necessary to run the job agent.")
+                    exit(1)
+                if yaml_data["pbp_job_agent"]["title"] is not None: # If the yaml file has a recorder key present.
+                    pass
+                else:
+                    logger.error("The 'title' key-value pair in the --global-attrs YAML file(s) is necessary to run the job agent.")
+                    exit(1)
+                if yaml_data["pbp_job_agent"]["cmlim"] is not None: # If the yaml file has a recorder key present.
+                    pass
+                else:
+                    logger.error("The 'cmlim' key-value pair in the --global-attrs YAML file(s) is necessary to run the job agent.")
+                    exit(1)
+                if yaml_data["pbp_job_agent"]["ylim"] is not None: # If the yaml file has a recorder key present.
+                    pass
+                else:
+                    logger.error("The 'ylim' key-value pair in the --global-attrs YAML file(s) is necessary to run the job agent.")
+                    exit(1)
+                if yaml_data["pbp_job_agent"]["meta_output_dir"] is not None: # If the yaml file has a recorder key present.
+                    pass
+                else:
+                    logger.error("The 'meta_output_dir' key-value pair in the --global-attrs YAML file(s) is necessary to run the job agent.")
+                    exit(1)
+                    
+            return yaml_data["pbp_job_agent"]
