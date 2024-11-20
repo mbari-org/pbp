e8f21ffd5ecebe6e2b87ba171ea23ccbd27eb456
Michael.C Ryan <spacetime.engineer@gmail.com>
2024-11-20 16:42:48 -0500

    Added multiprocessing
    Added multiprocessing

--- pbp/job_agent/main_job_agent.py
+++ pbp/job_agent/main_job_agent.py
@@ -1,36 +1,51 @@
 from pbp.job_agent.main_job_agent_args import parse_arguments
-
+from multiprocessing import Process
 def main():
     
-    opts = parse_arguments()
+    deployment_configurations = parse_arguments()
 
     from pbp.job_agent.job_agent import JobAgent
+    processes = []
+    for deployment_configuration in deployment_configurations:
 
-    job_agent = JobAgent(
-        output_prefix=opts["output_prefix"],
-        recorder=opts["recorder"],
-        audio_base_dir=opts["audio_base_dir"],
-        json_base_dir=opts["json_base_dir"],
-        start=opts["start"],
-        end=opts["end"],
-        prefix=opts["prefix"],
-        nc_output_dir=opts["nc_output_dir"],
-        global_attrs=opts["global_attrs"],
-        variable_attrs=opts["variable_attrs"],
-        sensitivity_flat_value=opts["sensitivity_flat_value"],
-        latlon=opts["latlon"],
-        title=opts["title"],
-        cmlim=opts["cmlim"],
-        ylim=opts["ylim"],
-        log_dir=opts["log_dir"],
-        meta_output_dir=opts["meta_output_dir"],
-        xml_dir=opts["xml_dir"]
-    )
+        job_agent = JobAgent(
+            output_prefix=deployment_configuration["output_prefix"],
+            recorder=deployment_configuration["recorder"],
+            audio_base_dir=deployment_configuration["audio_base_dir"],
+            json_base_dir=deployment_configuration["json_base_dir"],
+            start=deployment_configuration["start"],
+            end=deployment_configuration["end"],
+            prefix=deployment_configuration["prefix"],
+            nc_output_dir=deployment_configuration["nc_output_dir"],
+            global_attrs=deployment_configuration["global_attrs"],
+            variable_attrs=deployment_configuration["variable_attrs"],
+            sensitivity_flat_value=deployment_configuration["sensitivity_flat_value"],
+            latlon=deployment_configuration["latlon"],
+            title=deployment_configuration["title"],
+            cmlim=deployment_configuration["cmlim"],
+            ylim=deployment_configuration["ylim"],
+            log_dir=deployment_configuration["log_dir"],
+            meta_output_dir=deployment_configuration["meta_output_dir"],
+            xml_dir=deployment_configuration["xml_dir"]
+        )
+        
+        
+        pbp_job_agent_process = Process(target = job_agent.run)
+        processes.append(pbp_job_agent_process)
+        
     
-    try:
-        job_agent.run()
-    except KeyboardInterrupt as error:
-        print(f"Operation was interrupted by the user: {error}")
+
+    for process in processes:
+        process.start()
+            
+    for process in processes:
+        try:
+            process.join()
+        except KeyboardInterrupt as error:
+            print(f"Operation was interrupted by the user: {error}")
+            process.terminate()
+            process.join()
+
 
 if __name__ == "__main__":
     main()
